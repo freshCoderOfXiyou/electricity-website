66ac429640e5a479b04100a200bdeef165d731f3
freshCodeOfXiyou <18829291427@163.com>
2016-11-20 23:10:57 +0800

    This commit have finish sidebar function,and begin nav layout. 这次提交完成了sidebar的功能，并进行了nav的布局。

--- public/script/common.js
+++ public/script/common.js
@@ -8,6 +8,8 @@
 			var $toUpButton=$("#toUpButton")
 			var $userButton=$('.menubarUp .glyphicon-user')
 			var $menubar=$('.menubar')
+			var sideshow=false
+			var $detailDivs=$(".detailDiv")
 			// $menubar.hide()
 			// console.log($userButton.innerHTML())
 			window.addEventListener('scroll', function () {
@@ -48,18 +50,78 @@
 			$menubar.bind('click',function(event) {
 				var sourceElement=event.target||window.event.srcElement
 				var sourceName=sourceElement.className
-				if (sourceName=='.glyphicon .glyphicon-chevron-up') {
+				if (sourceName=='glyphicon glyphicon-chevron-up') {
 					return
 				}
 				else{
 					$('.fixSidebar').addClass('fixSidebarShow')
+					sideshow=true
+					
+					switch (sourceName) {
+						case 'glyphicon glyphicon-user':
+							for (var i = $detailDivs.length - 1; i >= 0; i--) {
+								$detailDivs[i].className='detailDiv'
+							}
+							$('#user').addClass('detailDivShow')
+							break;
+						case 'glyphicon glyphicon-shopping-cart':
+							for (var i = $detailDivs.length - 1; i >= 0; i--) {
+								$detailDivs[i].className='detailDiv'
+							}
+							$('#shoppingCart').addClass('detailDivShow')
+							break;
+						case 'glyphicon glyphicon-heart-empty':
+							for (var i = $detailDivs.length - 1; i >= 0; i--) {
+								$detailDivs[i].className='detailDiv'
+							}
+							$('#heart').addClass('detailDivShow')
+							break;
+						case 'glyphicon glyphicon-map-marker':
+							for (var i = $detailDivs.length - 1; i >= 0; i--) {
+								$detailDivs[i].className='detailDiv'
+							}
+							$('#map').addClass('detailDivShow')
+							break;
+						case 'glyphicon glyphicon-bell':
+							for (var i = $detailDivs.length - 1; i >= 0; i--) {
+								$detailDivs[i].className='detailDiv'
+							}
+							$('#bell').addClass('detailDivShow')
+							break;
+						case 'glyphicon glyphicon-edit':
+							for (var i = $detailDivs.length - 1; i >= 0; i--) {
+								$detailDivs[i].className='detailDiv'
+							}
+							$('#edit').addClass('detailDivShow')
+							break;
+
+						default:
+							
+							break;
+					}
 				}
 			})
 
 			// $('body').bind('click',function  () {
-			// 	$('.fixSidebar').removeClass('fixSidebarShow')
+			// 	var sourceElement=event.currentTarget||window.event.srcElement
+			// 	// if (sourceElement.className=='fixSidebar') {
+			// 	// 	return
+			// 	// 	console.log('123')
+			// 	// }
+			// 	// else{
+			// 	// 	$('.fixSidebar').removeClass('fixSidebarShow')
+			// 	// 	console.log('456')
+			// 	// 	console.log(sourceElement.className+"789")
+			// 	// }
+			// 	if (sideshow) {
+			// 		$('.fixSidebar').removeClass('fixSidebarShow')
+			// 		sideshow=false
+			// 		console.log('close')
+			// 	}
 			// })
-
+			$('.removeIcon').bind('click',function () {
+				$('.fixSidebar').removeClass('fixSidebarShow')
+			})
 
 
 
