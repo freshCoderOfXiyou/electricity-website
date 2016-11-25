3eefb8ad555b17acdd78b18183c4ef655047efbd
freshCodeOfXiyou <18829291427@163.com>
2016-11-25 21:14:45 +0800

    对有侧进行完善

--- public/script/index.js
+++ public/script/index.js
@@ -1,3 +1,10 @@
 (function () {
 	$('#navDetails').css('display','block')
+
+	// var $img=$("#bgimg")
+	// var $telCharge=$('.telCharge_icon')
+	// $telCharge.css('background-image',$img)
+	$('.telCharge_icon').hover(function () {
+		this.css('background','red')
+	})
 })()
