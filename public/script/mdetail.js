ff22f09e086f8e8669d109b4677a9d9bfa14eddf
江杨阳 <18829291427@163.com>
2017-05-01 21:56:13 +0800

    Update mdetail.js

--- public/script/mdetail.js
+++ public/script/mdetail.js
@@ -15,115 +15,127 @@
 		$('.middle').css("height" , infoH)
 		$(".middle-outer").css("height" , infoH)
 		console.log(infoH)
-		$('#header-info').click(function () {
-			// console.log($(this).attr('id'))
-			if (header_focus_item==$(this).attr('id')) {
-				return
-			}
-			clickbtn=1
-			// debugger
-			slide(focus_id,clickbtn)
-			focus_id=1
-			// $('.middle').css('left','0rem')
-			$(this).addClass('header-focus')
-			$("#"+header_focus_item).removeClass('header-focus')
-			header_focus_item='header-info'
-		})
+//点击头部标题“基本信息”的监听事件
+$('#header-info').click(function () {
+//若点击此时显示选项卡自己的按钮，则不执行任何操作
+if (header_focus_item==$(this).attr('id')) {
+	return
+}
+clickbtn=1
+//调用slide函数，传入的值为当前的选项卡id以及目标的id
+slide(focus_id,clickbtn)
+focus_id=1
+//改变此按钮的样式，是用户能够清楚了解此时查看的是哪一个选项卡
+$(this).addClass('header-focus')
+//改变此前选中选项卡对应按钮的样式，使其成为普通选项卡按钮
+$("#"+header_focus_item).removeClass('header-focus')
+header_focus_item='header-info'
+})
 
-		$('#header-more').click(function () {
-			if (header_focus_item==$(this).attr('id')) {
-				return
-			}
-			// debugger
-			clickbtn=2
-			slide(focus_id,clickbtn)
-			focus_id=2
-			// $('.middle').css('left','-10rem')
-			$(this).addClass('header-focus')
-			$("#"+header_focus_item).removeClass('header-focus')
-			header_focus_item='header-more'
-		})
+//点击头部“商品详情”的监听事件，执行的操作和点击“基本信息”类同
+$('#header-more').click(function () {
+if (header_focus_item==$(this).attr('id')) {
+	return
+}
+clickbtn=2
+slide(focus_id,clickbtn)
+focus_id=2
+$(this).addClass('header-focus')
+$("#"+header_focus_item).removeClass('header-focus')
+header_focus_item='header-more'
+})
 
-		$('#header-comment').click(function () {
-			if (header_focus_item==$(this).attr('id')) {
-				return
-			}
-			// debugger
-			clickbtn=3
-			slide(focus_id,clickbtn)
-			focus_id=3
-			// $('.middle').css('left','-20rem')
-			$(this).addClass('header-focus')
-			$("#"+header_focus_item).removeClass('header-focus')
-			header_focus_item='header-comment'
-
-		})
+//点击头部“评价”的监听事件，执行的操作和点击“基本信息”类同
+$('#header-comment').click(function () {
+if (header_focus_item==$(this).attr('id')) {
+	return
+}
+clickbtn=3
+slide(focus_id,clickbtn)
+focus_id=3
+$(this).addClass('header-focus')
+$("#"+header_focus_item).removeClass('header-focus')
+header_focus_item='header-comment'
+})
 		$("#header-comment").click()
-		//function slide 
-		function slide(nowPosId,nextPosId) {
-			var dis=nextPosId - nowPosId
-			// console.log(now_c)
-			switch(dis){
-				case 1:
-					//slide to left 10rem
-					move(-1 , screenWidth)
-					break
-				case -1:
-					//slide to right 10rem
-					move(1 , screenWidth)
-					break
-				case 2:
-					//slide to left 20rem
-					move(-1 , 2 * screenWidth)
-					break
-				case -2:
-					//slide to right 20rem
-					move(1 , 2 * screenWidth)
-					break
-			}
-		}
 
-		//@param direc :-1 mean sliding to left,1 mean sliding to right
-		function move( direc , move_lenth) {
-			var move_dis=0
-			// var move_step=1
-			var move_time=20
-			var now_css_left=parseInt($('.middle').css('left'))
-			// console.log(now_css_left)
-			var timer=setInterval(function () {
+		
+//在此函数中使用switch语句来进行判断可以使用更加简洁的代码，直接调用函数move并传入dis值，需要简单调整下move函数的实现
+//function slide 
+//根据起始的位置以及最终的位置来调用函数move
+function slide(nowPosId,nextPosId) {
+//获取当前点击的头部按钮的id值与此前选中头部按钮的id值
+//使用两者相减得到差值
+var dis=nextPosId - nowPosId
+//使用switch语句根据不同的差值执行不同的调用
+switch(dis){
+	case 1:
+		//slide to left 10rem
+		//如果差值为1则向左滑动一屏的距离
+		move(-1 , screenWidth)
+		break
+	case -1:
+		//slide to right 10rem
+		//如果差值为-1则右滑动一屏距离
+		move(1 , screenWidth)
+		break
+	case 2:
+		//slide to left 20rem
+		//如果差值为2则向左滑动两屏距离
+		move(-1 , 2 * screenWidth)
+		break
+	case -2:
+		//slide to right 20rem
+		//如果差值为-2则向右滑动两屏距离
+		move(1 , 2 * screenWidth)
+		break
+}
+}
 
-						now_css_left += direc * setpmove
-						move_dis +=setpmove
-						$('.middle').css('left',now_css_left + 'px')
-						// console.log("left:"+now_css_left)
-						// console.log("actually move:"+move_dis)
-						// console.log("expect move"+move_lenth)
-						if (Math.abs(move_dis - move_lenth) < 0.001 ) {
-							clearInterval(timer)
-							// console.log(move_dis)
-							switch(focus_id){
-								case(1):
-									$('.middle').css('left','0rem').css("height" , infoH)
-									$(".middle-outer").css("height" , infoH)
-									console.log($(".middle-outer").css("height"))
-									break
-								case(2):
-									$('.middle').css('left','-10rem').css("height" , moreH)
-									$(".middle-outer").css("height" , moreH)
-									console.log($(".middle-outer").css("height"))
-									break
-								case(3):
-									$('.middle').css('left','-20rem').css("height" , commentH)
-									$(".middle-outer").css("height" , commentH)
-									console.log($(".middle-outer").css("height"))
-									break
-								default:
-									break
-							}
-							window.scrollTo(0,0)
-						}		
-					},move_time)//end timer
-		}//end func move
+//@param direc :-1 mean sliding to left,1 mean sliding to right
+//执行具体的元素移动的函数，direc表示方向，-1表示向左滑动，1表示向右滑动
+function move( direc , move_lenth) {
+//用于记录移动的距离，当移动距离达到参数时停止计时器
+var move_dis=0
+//计时器的执行间隔时间
+var move_time=20
+//获取父元素的left值
+var now_css_left=parseInt($('.middle').css('left'))
+//开始执行计时器，使用一个作用于限于函数的变量来保存计时器对象
+var timer=setInterval(function () {
+//计算得到一次执行中的left值，当direc带有正负的信息可以决定移动的方向
+now_css_left += direc * setpmove
+//计算累计移动距离
+move_dis +=setpmove
+//设置父元素的left值
+$('.middle').css('left',now_css_left + 'px')
+//当移动完毕时，根据被点击的按钮来设置父元素的高度
+if (Math.abs(move_dis - move_lenth) < 0.001 ) {
+clearInterval(timer)
+switch(focus_id){
+case(1):
+	$('.middle').css('left','0rem').css("height" , infoH)
+	$(".middle-outer").css("height" , infoH)
+	console.log($(".middle-outer").css("height"))
+	break
+case(2):
+	$('.middle').css('left','-10rem').css("height" , moreH)
+	$(".middle-outer").css("height" , moreH)
+	console.log($(".middle-outer").css("height"))
+	break
+case(3):
+	$('.middle').css('left','-20rem').css("height" , commentH)
+	$(".middle-outer").css("height" , commentH)
+	console.log($(".middle-outer").css("height"))
+	break
+default:
+	break
+}
+//将页面设置到最顶部，因为子元素高度不统一的缘故
+window.scrollTo(0,0)
+}		
+},move_time)//end timer
+}//end func move
 		// slide(1,2)
 
 		// @begin finger js
